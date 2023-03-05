c6a56d4f6923c1a7971714e1db3c2f28ad2eff54
ukota09 <123261012+ukota09@users.noreply.github.com>
2023-03-05 13:57:36 -0600

    Update ex_5_2.py
    completed

--- src/ex_5_2.py
+++ src/ex_5_2.py
@@ -14,7 +14,7 @@ except ImportError:
     from util import get_repository_root
 
 
-if __name__ == "__main__":
+if _name_ == "_main_":
 
     # Use these predefined input / output files
     root_dir = get_repository_root()
@@ -22,5 +22,9 @@ if __name__ == "__main__":
     OUTFILE = root_dir / "outputs" / "ex_5_2-processed.csv"
 
     # Complete the data processing steps using numpy here.
-
+    data = np.loadtxt(INFILE, delimiter=',')
+    data -= np.mean(data)
+    data /= np.std(data)
+    processed = data
     # Save the output to OUTFILE using numpy routines.
+    np.savetxt(OUTFILE, processed, delimiter=',')
