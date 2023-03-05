0d6debc16951c1cef630234244bb33f89a8f2837
ukota09 <123261012+ukota09@users.noreply.github.com>
2023-03-05 14:00:12 -0600

    Update ex_5_3.py
    completed

--- src/ex_5_3.py
+++ src/ex_5_3.py
@@ -8,7 +8,7 @@ This module contains an entry point that:
 import numpy as np
 from argparse import ArgumentParser
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     # Create your argument parser object here.
     # Collect the filename arguments from the command line
     # Rewrite your 5_3 logic here so that it utilizes the arguments passed from the command line.
@@ -16,4 +16,12 @@ if __name__ == "__main__":
     # Tests will run your command using a system call.
     # To test your program with arguments, run it from the command line
     # (see README.md for more details)
-    pass
+    parser = ArgumentParser(description='This program applies a standard scale transform to the data in infile and writes it to outfile.')
+    parser.add_argument('infile', help = 'input file name')
+    parser.add_argument('outfile', help = 'output file name')
+    args = parser.parse_args()
+    data = np.loadtxt(args.infile, delimiter=',')
+    data -= np.mean(data)
+    data /= np.std(data)
+    processed = data
+    np.savetxt(args.outfile, processed, delimiter=',')
