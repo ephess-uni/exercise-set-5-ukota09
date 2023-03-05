14e40187b77de8888c5086ed0bdab59a3c92cec9
ukota09 <123261012+ukota09@users.noreply.github.com>
2023-03-05 16:29:23 -0600

    Update ex_5_3.py

--- src/ex_5_3.py
+++ src/ex_5_3.py
@@ -1,14 +1,18 @@
 """ ex_5_3.py
 This module contains an entry point that:
-
 - creates a CLi that accepts an input file of data to be processed
 - shifts and scales the data to a mean of 0 and a standard deviation 1
 - writes the file to the output file argument
 """
 import numpy as np
-from argparse import ArgumentParser
-
-if _name_ == "_main_":
+import argparse
+import os
+try:
+    from src.util import get_repository_root
+except ImportError:
+    from util import get_repository_root
+    
+if __name__ == "__main__":
     # Create your argument parser object here.
     # Collect the filename arguments from the command line
     # Rewrite your 5_3 logic here so that it utilizes the arguments passed from the command line.
@@ -16,12 +20,26 @@ if _name_ == "_main_":
     # Tests will run your command using a system call.
     # To test your program with arguments, run it from the command line
     # (see README.md for more details)
-    parser = ArgumentParser(description='This program applies a standard scale transform to the data in infile and writes it to outfile.')
-    parser.add_argument('infile', help = 'input file name')
-    parser.add_argument('outfile', help = 'output file name')
+    desc = "This program applies a standard scale transform to the data in infile and writes it to outfile."
+    
+    root_dir = get_repository_root()
+    
+    os.makedirs(root_dir / "outputs", exist_ok=True)
+    
+    parser = argparse.ArgumentParser(description=desc)
+
+    parser.add_argument("infile", type=argparse.FileType('r'))
+
+    parser.add_argument("outfile", type=argparse.FileType('w'))
+
     args = parser.parse_args()
-    data = np.loadtxt(args.infile, delimiter=',')
-    data -= np.mean(data)
-    data /= np.std(data)
-    processed = data
-    np.savetxt(args.outfile, processed, delimiter=',')
+
+    data_in_file = np.loadtxt(args.infile)
+
+    data_in_file -= data_in_file.mean()
+
+    SD = data_in_file.std()
+
+    processed=data_in_file/SD
+
+    np.savetxt(args.outfile, processed, fmt='%.2e')
