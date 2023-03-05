224902d92636e34eb433d97068b8921273670a40
ukota09 <123261012+ukota09@users.noreply.github.com>
2023-03-05 13:48:54 -0600

    Update ex_5_1.py
    completed

--- src/ex_5_1.py
+++ src/ex_5_1.py
@@ -4,17 +4,22 @@ try:
 except ImportError:
     from ex_5_0 import line_count
 
+import argparse
 
 def main(infile):
     """Call line_count with the infile argument."""
     line_count(infile)
 
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     # Create your argument parser object here.
     # Collect the filename argument from the command line
     # pass this argument to the main function above
     # Tests will run your command using a system call.
     # To test your program with arguments, run it from the command line
     # (see README.md for more details)
-    pass
+    parser = argparse.ArgumentParser(description='This program prints the number of lines in infile.')
+    parser.add_argument('filename',
+                        help = 'Filename to count the lines in it')
+    args = parser.parse_args()
+    main(args.filename)
