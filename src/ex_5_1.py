bf682b54edc4781626e165ebb47609cf428877e1
ukota09 <123261012+ukota09@users.noreply.github.com>
2023-03-05 14:02:02 -0600

    Update ex_5_1.py
    completed

--- src/ex_5_1.py
+++ src/ex_5_1.py
@@ -14,7 +14,7 @@ def main(infile):
 if _name_ == "_main_":
     # Create your argument parser object here.
     # Collect the filename argument from the command line
-    # pass this argument to the main function above
+    # pass this argument    to the main function above
     # Tests will run your command using a system call.
     # To test your program with arguments, run it from the command line
     # (see README.md for more details)
